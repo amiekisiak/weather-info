a01dc1be85eddb9e4036b87662532affa92ae473
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-01-29 19:45:11 +0000

    added changes to js

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,57 @@
 // Add your own API key between the ""
+
+
 var APIKey = "a8dd88d399c7287d32254917cfc9e273";
+var citiesListEl = ["London", "Paris", "New York", "Tokyo"];
+var currentDay = document.querySelector("#today-date");
+let now = moment();
+currentDay.innerHTML = now.format("DD/MM/YYYY");
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citiesListEl + "&appid=" + APIKey;
 
-var searchButton = $("#search-button");
+  
 
-// Here we are building the URL we need to query the database
-
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "&appid=" + APIKey;
 // We then created an AJAX call
 $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    // Create CODE HERE to Log the queryURL
-    console.log(queryURL);
-    // Create CODE HERE to log the resulting object
-    console.log(response);
-    var celsiusTemperature = Math.floor(response.main.temp - 273.15);
-  // Create CODE HERE to transfer content to HTML
-  var myTempDiv = $('.temp');
-  myTempDiv.text(celsiusTemperature + " degrees Celsius");
-  var city = response.name;
-  var humidity = response.main.humidity;
-  var wind = response.wind.speed;
-  $('.city').text("Weather for " + city);
-  $('.humidity').text("Relative humidity: " + humidity + "%");
-  $('.wind').text("Wind speed: " + wind + " m/s");
-  // Hint: To convert from Kelvin to Celsius: C = K - 273.15
-  // Create CODE HERE to dump the temperature content into HTML
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(queryURL);
+  // Create CODE HERE to log the resulting object
+  console.log(response);
+  // Create CODE HERE to calculate the temperature (converted from Kelvin)
+
+  $('.city').html("<h2>" + response.name + "</h2>");
+  $('.weather-icon').html("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' >");
+  $('.wind').text("Wind Speed: " + response.wind.speed + " MPH");
+  $('.humidity').text("Humidity: " + response.main.humidity + "%");
+  $(".temperature").text("Temperature: " + response.main.temp + " F");
 
 });
+
+function renderButtons() {
+
+  // Deleting the movie buttons prior to adding new movie buttons
+  // (this is necessary otherwise we will have repeat buttons)
+  $("#buttons-view").empty();
+
+  // Looping through the array of movies
+  for (var i = 0; i < citiesListEl.length; i++) {
+
+    // Then dynamicaly generating buttons for each movie in the array.
+    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adding a class
+    a.addClass("city");
+    // Adding a data-attribute with a value of the movie at index i
+    a.attr("data-name", citiesListEl[i]);
+    // Providing the button's text with a value of the movie at index i
+    a.text(citiesListEl[i]);
+    // Adding the button to the HTML
+    $("#buttons-view").append(a);
+  }
+}
+
+
+
+
+
