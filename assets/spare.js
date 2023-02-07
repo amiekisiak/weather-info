8a56cb369e29d71c7ef10854cee748d0bce53c43
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-02-07 14:59:48 +0000

    added gitignore file

--- assets/spare.js
+++
@@ -1,5 +0,0 @@
-// Fetch weather for a city
-async function getTodayWeather(city) {
-    const response = await fetch(`${weatherUrl}${city}&appid=${apiKey}`);
-    const data = await response.json();
-  
